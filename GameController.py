ba35cac161c73946cf551975133d0df012dfd735
Frieda <buchner@short-cuts.de>
2022-05-06 23:18:23 +0200

    stage 1 of java to python portation completed

--- GameController.py
+++ GameController.py
@@ -1,5 +1,7 @@
 from GameboardDrawer import GameboardDrawer
 from Match import Match
+from MatchController import MatchController
+from Symbol import Symbol
 
 
 class GameController:
@@ -8,88 +10,50 @@ class GameController:
         self.game_is_over = bool(False)
 
     def run_tic_tac_toe(self):
-        self.ask_player_which_symbol()
+        self.ask_player_which_symbol(self)
         while (self.user_cant_get_enough()):
             match = Match(self.player_symbol)
-            self.print_instructions();
+            self.print_instructions(self);
             GameboardDrawer.draw_gameboard(match.grid)
-            matchController = MatchController(match);
-            matchController.start_match();
-
+            match_controller = MatchController(match)
+            match_controller.start_match()
         else:
             print("Bye.")
             exit()
 
-def user_cant_get_enough(self):
-{
-System.out.println("Do you want to play again? [re][restart] [ex][exit]");
-BufferedReader
-bufferedReader = new
-BufferedReader(new
-InputStreamReader(System. in));
-try {
-System.out.print(">_");
-String input = bufferedReader.readLine();
-if (input.equals("restart") | | input.equals("re")) {
-return true;
-} else if (input.equals("exit") | | input.equals("ex")) {
-return false;
-} else {
-System.out.println("Please, play by the rules. Try again.");
-}
-} catch(IOException
-e) {
-    System.out.println("Error. Try again.");
-userCantGetEnough();
-}
-return false;
-}
+    def user_cant_get_enough(self):
+        print("Do you want to play again? [re][restart] [ex][exit]")
+        print(">_")
+        user_input = input()
+        if user_input == "restart" or user_input == "re":
+            return bool(True)
+        elif user_input == "exit" or user_input == "ex":
+            return bool(False)
+        else:
+            print("Please, play by the rules. Try again.")
+        return bool(False)
 
     def ask_player_which_symbol(self):
-BufferedReader
-bufferedReader = new
-BufferedReader(new
-InputStreamReader(System. in));
-System.out.print("Hello!\nAre you X or O?: [X] [O]\n");
-try {
-System.out.print(">_");
-String input = bufferedReader.readLine();
-if (checkCharacterForValidity(input)) {
-saveChosenCharacter(input);
-} else {
-System.out.println("Try again.");
-askPlayerWhichSymbol();
-}
-} catch(IOException e) {
-System.out.println("Error. Try again.");
-askPlayerWhichSymbol();
-}
-}
+        print("Hello!\nAre you X or O?: [X] [O]\n")
+        print(">_")
+        user_input = input()
+        if self.check_character_for_validity(self,user_input):
+            self.save_chosen_character(self, user_input)
+        else:
+            print("Try again.")
+            self.ask_player_which_symbol(self);
 
-private
-void
-printInstructions()
-{
-System.out.print("You are " + playerSymbol + ". ");
-System.out.print("You start.\n");
-System.out.println("Enter your field coordinate.");
-}
+    def print_instructions(self):
+        print("You are " + self.player_symbol + ". ")
+        print("You start.\n")
+        print("Enter your field coordinate.")
 
-private
-void
-saveChosenCharacter(String
-input) {
-if ((input.equals("X")) | | (input.equals("x"))) {
-playerSymbol = Symbol.X;
-} else if ((input.equals("O")) | | (input.equals("o"))) {
-playerSymbol = Symbol.O;
-}
-}
+    def save_chosen_character(self, user_input):
+        if (user_input == "X") or (user_input == "x"):
+            self.player_symbol = Symbol.X
+        elif (user_input == "O") or (user_input == "o"):
+            self.player_symbol = Symbol.O
 
-private
-boolean
-checkCharacterForValidity(String
-input) {
-return ((input.equals("X")) | | (input.equals("x")) | | (input.equals("O")) | | (input.equals("o")));
-}
-}
+    def check_character_for_validity(user_input):
+        return (user_input == "X") or (user_input == "x") or (user_input == "O") or (user_input == "o")
+
